c0b000efd5f8c7967eda9e7912ce17fb20d7498d
Hector Avalos <gerardo.avalos97@hotmail.com>
2020-03-28 18:51:09 -0600

    Added both linux and windows avr toolchains, loaded system commands from json file and added a fallback

--- Readme.md
+++ Readme.md
@@ -3,17 +3,17 @@
 
 ## Pre-requisites:
 ##### Linux installation (Ubuntu 18.04)
-* sudo apt install build-essential binutils gcc-avr avr-libc uisp flex bison byacc
+`sudo apt install build-essential binutils gcc-avr avr-libc uisp flex bison byacc`
 
 ### Usage:
-* avr-compile.py -s/--source -m/--micro [-o/--output]
+`avr-compile.py -s/--source -m/--micro [-o/--output]`
 
 ## Examples:
-* avr-compile.py --source rotabit.c --micro atmega32a
-* avr-compile.py --source rotabit.c --micro atmega8 --output rotabit
+`avr-compile.py --source example.c --micro atmega32a`
+`avr-compile.py --source example.c --micro atmega8 --output example_output`
 
 ### Output
-###### This compiler produces
+###### This compiler produces the following files, in the same directory where the *.c source code file is located.
 * *.o file
 * *.elf file
 * *.hex file
--- avr-compile.py
+++ avr-compile.py
@@ -9,40 +9,95 @@ __status__ = "Production"
 import argparse
 
 
+def get_instruction_set(operating_system):
+	import json
+
+	instruction_set = {}
+
+	try:
+		with open("instruction_set.jso") as fp:
+			lines = fp.read()
+			instructions = json.loads(lines)
+
+			if operating_system == "Linux":
+				instruction_set = { name:instr for (name, instr) in instructions["Linux"].items() }
+			else:
+				instruction_set = { name:instr for (name, instr) in instructions["Windows"].items() }
+	except:
+		print("Instructions not found, using fallback values...")
+	return instruction_set
+
+
+def fallback_instruction_set(operating_system):
+	instruction_set = {}
+
+	if operating_system == "Linux":
+		instruction_set["c_to_obj"] = './avr8-gnu-toolchain-linux/bin/avr-gcc -g -Os -mmcu={MICROCONTROLLER} -c {SOURCEFILEPATH}{SOURCEFILE}.c'
+		instruction_set["obj_to_elf"] = './avr8-gnu-toolchain-linux/bin/avr-gcc -g -mmcu={MICROCONTROLLER} -o {SOURCEFILEPATH}{SOURCEFILE}.elf {SOURCEFILEPATH}{SOURCEFILE}.o'
+		instruction_set["elf_to_hex"] = './avr8-gnu-toolchain-linux/bin/avr-objcopy -j .text -j .data -O ihex {SOURCEFILEPATH}{SOURCEFILE}.elf {SOURCEFILEPATH}{OUTPUTFILE}.hex'
+		instruction_set["get_filesize"] = './avr8-gnu-toolchain-linux/bin/avr-size --format=avr --mcu={MICROCONTROLLER} {SOURCEFILEPATH}{SOURCEFILE}.elf'
+		instruction_set["mv_obj_to_srcpath"] = 'mv ./{SOURCEFILE}.o {SOURCEFILEPATH}'
+	else:
+		instruction_set["c_to_obj"] = './avr8-gnu-toolchain-win32/bin/avr-gcc.exe -g -Os -mmcu={MICROCONTROLLER} -c {SOURCEFILEPATH}{SOURCEFILE}.c'
+		instruction_set["obj_to_elf"] = './avr8-gnu-toolchain-win32/bin/avr-gcc.exe -g -mmcu={MICROCONTROLLER} -o {SOURCEFILEPATH}{SOURCEFILE}.elf {SOURCEFILEPATH}{SOURCEFILE}.o'
+		instruction_set["elf_to_hex"] = './avr8-gnu-toolchain-win32/bin/avr-objcopy.exe -j .text -j .data -O ihex {SOURCEFILEPATH}{SOURCEFILE}.elf {SOURCEFILEPATH}{OUTPUTFILE}.hex'
+		instruction_set["get_filesize"] = './avr8-gnu-toolchain-win32/bin/avr-size.exe --format=avr --mcu={MICROCONTROLLER} {SOURCEFILEPATH}{SOURCEFILE}.elf'
+		instruction_set["mv_obj_to_srcpath"] = 'move {SOURCEFILE}.o {SOURCEFILEPATH}'
+	return instruction_set
+
+
 def do_compile(sourceFile, microcontroller, outputFile):
 	import os
+	import platform
+
+	source_file_location = os.path.abspath(sourceFile)
+	source_file_path, sourceFile = os.path.split(source_file_location)
+	source_file_path = "{}/".format(os.path.dirname(source_file_location))
 	sourceFile = sourceFile[0:-2] # Removes .c extension
+	instruction_set = {};
+
 	if not outputFile:
 		outputFile = sourceFile
 
-	os.system(
-		'avr-gcc -g -Os -mmcu={MICROCONTROLLER} -c {SOURCEFILE}.c'
-		.format(
+	instruction_set = get_instruction_set(platform.system())
+	if len(instruction_set.items()) == 0:
+		instruction_set = fallback_instruction_set(platform.system())
+
+	print(
+		instruction_set["c_to_obj"].format(
 			MICROCONTROLLER=microcontroller,
+			SOURCEFILEPATH=source_file_path,
 			SOURCEFILE=sourceFile
 		)
 	)
 
-	os.system(
-		'avr-gcc -g -mmcu={MICROCONTROLLER} -o {SOURCEFILE}.elf {SOURCEFILE}.o'
-		.format(
-			MICROCONTROLLER=microcontroller,
-			SOURCEFILE=sourceFile
-		)
-	)
-
-	os.system(
-		'avr-objcopy -j .text -j .data -O ihex {SOURCEFILE}.elf {OUTPUTFILE}.hex'
-		.format(
+	print(
+		instruction_set["mv_obj_to_srcpath"].format(
 			SOURCEFILE=sourceFile,
+			SOURCEFILEPATH=source_file_path
+		)
+	)
+
+	print(
+		instruction_set["obj_to_elf"].format(
+			MICROCONTROLLER=microcontroller,
+			SOURCEFILEPATH=source_file_path,
+			SOURCEFILE=sourceFile
+		)
+	)
+
+	print(
+		instruction_set["elf_to_hex"].format(
+			SOURCEFILE=sourceFile,
+			SOURCEFILEPATH=source_file_path,
 			OUTPUTFILE=outputFile
 		)
 	)
 
-	os.system(
-		'avr-size --format=avr --mcu={MICROCONTROLLER} {SOURCEFILE}.elf'
-		.format(
+	print(
+		instruction_set["get_filesize"].format(
 			MICROCONTROLLER=microcontroller,
+			SOURCEFILEPATH=source_file_path,
 			SOURCEFILE=sourceFile
 		)
 	)
